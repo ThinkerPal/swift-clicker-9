118fef7fcd58a180f77f4d2787569f2cbdfcbc81
Rui Yang Tan <thinkerpal@thinker-pals-mbp.local>
2018-07-21 09:18:14 +0800

    Initial Commit

---
+++ Clicker Version 9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clicker Version 9
+//
+//  Created by Rui Yang Tan on 21/7/18.
+//  Copyright © 2018 Rui Yang Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
