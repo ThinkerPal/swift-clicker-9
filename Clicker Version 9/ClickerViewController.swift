ca33520bf7220600f6bbffffa72ecbfdd16c8894
Rui Yang Tan <thinkerpal@thinker-pals-mbp.local>
2018-07-21 09:25:47 +0800

    Logic added

--- Clicker Version 9/ClickerViewController.swift
+++ Clicker Version 9/ClickerViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ClickerViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        var timer = Timer(t)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func tapped(_ sender: Any) {
+        counter += 1
+        label.text = String(counter)
+        if counter == 30 {
+            print("Ypu're done! You took \(counter) secondss")
+        }
+    }
+    
 
 }
 
